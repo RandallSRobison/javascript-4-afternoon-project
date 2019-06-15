4605e94f410033e9a610cb363a111fd3f3ea5082
Spence Robison <spence@Spences-MacBook-Pro.local>
2019-06-14 18:02:21 -0600

    finished because i finished it

--- classes.js
+++ classes.js
@@ -30,7 +30,17 @@
 */
 
 //Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+  makeWidget() {
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -48,8 +58,18 @@
 */
 
 //Code Here
-
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(employee) {
+    this.reports.push(employee);
+  }
+  fire(index) {
+    this.reports.splice(index, 1);
+  }
+}
 ////////// PROBLEM 3 //////////
 
 /*
@@ -66,15 +86,45 @@
     51-100 reports : Manager Plus
     101+ reports : Bestest Manager
 
-  Everytime they fire an employee they get $100 added to their bonus.
+  Every time they fire an employee they get $100 added to their bonus.
 
   Call your new class ProgressiveManager
 */
 
 //Code Here
+class ProgressiveManager extends Manager {
+  constructor(first_name, last_name, email, age, reports, bonus, title) {
+    super(first_name, last_name, email, age, reports);
+    this.bonus = 0;
+    this.title = "Not a manager";
+  }
 
+  hire(employee) {
+    super.hire(employee);
+    this.updated();
+  }
+  fire(index) {
+    super.fire(index);
+    this.updated();
+    return (this.bonus += 100);
+  }
 
-
+  updated() {
+    if (this.reports.length >= 101) {
+      return (this.title = "Bestest Manager");
+    } else if (this.reports.length > 50) {
+      return (this.title = "Manager Plus");
+    } else if (this.reports.length > 10) {
+      return (this.title = "Manager");
+    } else if (this.reports.length > 3) {
+      return (this.title = "Mostly Manager");
+    } else if (this.reports.length <= 3) {
+      return (this.title = "Barely Manager");
+    } else if ((this.reports.length = 0)) {
+      return (this.title = "Not a Manager");
+    }
+  }
+}
 ////////// PROBLEM 4 - Black Diamond //////////
 
 /*
@@ -99,5 +149,3 @@
 */
 
 //Code Here
-
-
